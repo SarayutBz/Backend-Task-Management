bde9fb4dd094d1d9b3a6c1f6d7c95241da731eda
SarayutBz <sarayut.aiamaurai@gmail.com>
2026-04-30 10:59:44 +0700

    feat(auth): add login endpoint with JWT

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -27,5 +27,35 @@ router.post('/register',async (req,res,next) =>{
     }
 })
 
+router.post('/login',async(req,res,next) =>{
+    try{
+        const {email,password} = req.body
+        if(!email || !password)
+            return res.status(400).json({message:'Please fill out the email and password'})
+        
+        const [rows] = await pool.query('SELECT * FROM users WHERE email = ?',[email])
+        if(row.length === 0)
+            return res.status(401).json({message:'email or password incorrect'})
+
+        const user = rows[0]
+        const valid = await bcrypt.compare(password,user.password_hash)
+        if(!valid)
+            return res.status(401).json({message:'email or password incorrect'})
+
+
+        const token = jwt.sign(
+            {userId:user.id,email:user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:process.env.JWT_EXPIRES_IN }
+        )
+
+        res.json({
+            token,
+            user:{id:user.id,name:user.name,email:user.email}
+        })
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router
